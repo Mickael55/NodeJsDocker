fb67f7f7587a48e09b2e0cc25fbba775a84e9be7
Mickael55 <83458564+Mickael55@users.noreply.github.com>
2021-12-01 11:44:48 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+//var route = require('/html/index.html');
+
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json({extended : true});
+//app.use(jsonParser);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/html', express.static(__dirname + "/html"));
+app.get('/', function (req, res) {
+    res.redirect('/html/index.html');
+});
+
+
+
+
+
+app.post('/add', function (request, response) {
+    var p1 = request.body.p1;
+   // console.log(req);
+    console.log(p1);
+    
+   // res.send(a);
+    response.send(p1);
+  
+    //res.redirect('/html/index.html');
+});
+
+app.listen(8282, function () {
+    console.log("http://localhost:8282");
+});
